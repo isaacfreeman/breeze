fc47a45fe487c48f3a74bba00a81be174a4e562f
Isaac Freeman <isaac@leftclick.com>
2013-05-07 15:39:28 +1200

    playing with module load order

--- lib/breeze.rb
+++ lib/breeze.rb
@@ -12,6 +12,7 @@ require "breeze/queueing/resque"
 require "breeze/queueing/synchronous"
 require "breeze/queueing"
 require "breeze/theming"
+require "breeze/content"
 # Next we load the model
 # The following paragraph contains the loading of our model, looks quite messy but it resolve dependencies
 # note the require function does not require twice file, but identity if it is already in the LOAD_PATH
@@ -29,7 +30,6 @@ require File.expand_path("./../app/models/breeze/content/asset.rb", File.dirname
 require File.expand_path("./../app/models/breeze/content/custom/field.rb", File.dirname(__FILE__))
 
 Dir[File.expand_path("./../app/models/breeze/**/*.rb", File.dirname(__FILE__))].each { |f| require f }
-require "breeze/content"
 
 module Breeze
   extend self
@@ -39,13 +39,13 @@ module Breeze
   def config
     @config ||= Configuration.first || Configuration.create
   end
-  
+
   def with_domain domain
     old_domain, @_domain = @_domain, domain
     yield
     @_domain = old_domain
   end
-  
+
   def domain
     @_domain
   end
